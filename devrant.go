b9bc1484a61d73be781d08c7d12b201502516537
Jayesh <jayeshsolanki93@gmail.com>
2016-08-10 13:08:15 +0530

    Make app paths as string constants

--- devrant.go
+++ devrant.go
@@ -8,8 +8,15 @@ import (
 )
 
 const (
-	API         = "https://www.devrant.io/api"
-	APP_VERSION = 3
+	API           = "https://www.devrant.io/api"
+	APP_VERSION   = 3
+	RANTS_PATH    = "%s/devrant/rants?sort=%s&limit=%d&skip=%d&app=%d"
+	RANT_PATH     = "%s/devrant/rants/%d?app=%d"
+	USER_PATH     = "%s/users/%d?app=%d"
+	USER_ID_PATH  = "%s/get-user-id?username=%s&app=%d"
+	SEARCH_PATH   = "%s/devrant/search?term=%s&app=%d"
+	SURPRISE_PATH = "%s/devrant/rants/surprise?app=%d"
+	WEEKLY_PATH   = "%s/devrant/weekly-rants?app=%d"
 )
 
 type Client struct {
@@ -26,8 +33,7 @@ func (c *Client) Rants(sort string, limit int, skip int) ([]RantModel, error) {
 		limit = 50
 	}
 
-	url := fmt.Sprintf("%s/devrant/rants?sort=%s&limit=%d&skip=%d&app=%d",
-		API, sort, limit, skip, APP_VERSION)
+	url := fmt.Sprintf(RANTS_PATH, API, sort, limit, skip, APP_VERSION)
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -39,7 +45,7 @@ func (c *Client) Rants(sort string, limit int, skip int) ([]RantModel, error) {
 
 // Fetches a rant and its comments given a valid rant id
 func (c *Client) Rant(rantId int) (RantModel, []CommentModel, error) {
-	url := fmt.Sprintf("%s/devrant/rants/%d?app=%d", API, rantId, APP_VERSION)
+	url := fmt.Sprintf(RANT_PATH, API, rantId, APP_VERSION)
 	res, err := http.Get(url)
 	if err != nil {
 		return RantModel{}, nil, err
@@ -55,7 +61,7 @@ func (c *Client) Profile(username string) (UserModel, ContentModel, error) {
 	if err != nil {
 		return UserModel{}, ContentModel{}, err
 	}
-	url := fmt.Sprintf("%s/users/%d?app=%d", API, userId, APP_VERSION)
+	url := fmt.Sprintf(USER_PATH, API, userId, APP_VERSION)
 	res, err := http.Get(url)
 	if err != nil {
 		return UserModel{}, ContentModel{}, err
@@ -67,7 +73,7 @@ func (c *Client) Profile(username string) (UserModel, ContentModel, error) {
 
 // Search for rants matching the search term
 func (c *Client) Search(term string) ([]RantModel, error) {
-	url := fmt.Sprintf("%s/devrant/search?term=%s&app=%d", API, term, APP_VERSION)
+	url := fmt.Sprintf(SEARCH_PATH, API, term, APP_VERSION)
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -79,7 +85,7 @@ func (c *Client) Search(term string) ([]RantModel, error) {
 
 // Returns a random rant
 func (c *Client) Surprise() (RantModel, error) {
-	url := fmt.Sprintf("%s/devrant/rants/surprise?app=%d", API, APP_VERSION)
+	url := fmt.Sprintf(SURPRISE_PATH, API, APP_VERSION)
 	res, err := http.Get(url)
 	if err != nil {
 		return RantModel{}, err
@@ -91,7 +97,7 @@ func (c *Client) Surprise() (RantModel, error) {
 
 // Returns the rants tagged for 'weekly'
 func (c *Client) WeeklyRants() ([]RantModel, error) {
-	url := fmt.Sprintf("%s/devrant/weekly-rants?app=%d", API, APP_VERSION)
+	url := fmt.Sprintf(WEEKLY_PATH, API, APP_VERSION)
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -103,8 +109,7 @@ func (c *Client) WeeklyRants() ([]RantModel, error) {
 
 // Fetches the userId given a valid username
 func getUserId(username string) (int, error) {
-	url := fmt.Sprintf("%s/get-user-id?username=%s&app=%d",
-		API, username, APP_VERSION)
+	url := fmt.Sprintf(USER_ID_PATH, API, username, APP_VERSION)
 	res, err := http.Get(url)
 	if err != nil {
 		return 0, err
